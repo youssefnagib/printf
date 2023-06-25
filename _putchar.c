96018d69e77d6405087c93aedb668979f827ad76
Ma-rwan <marwaneltaieby@hotmail.com>
2023-06-25 11:48:18 +0000

    _putchar

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - write character c to stdout
+ * @c: the character
+ * Return: 1 on success and -1 otherwise
+ */
+
+int _putchar(char)
+{
+	return (write(1, &c, 1));
+}
