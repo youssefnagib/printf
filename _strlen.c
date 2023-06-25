b6e7290abfb4988ac1cd67de0020d792d575563b
Ma-rwan <marwaneltaieby@hotmail.com>
2023-06-25 21:29:34 +0000

    _strlen

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include <stdlib>
+
+/**
+ * _strlen - length of string
+ * @s: the string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (0);
+}
