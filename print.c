fdc97d1f88190a499437770fad44f1e039a0dfe9
youssefnagib <youssefnagib@yahoo.com>
2023-06-26 13:08:48 +0200

    yalla bena

---
+++ print.c
@@ -0,0 +1,139 @@
+#include "main.h"
+
+int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	char matrix = 0, padd = ' ';
+	int i = BUFF_SIZE - 2, length = 2, padd_start = 1;
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
+
+	UNUSED(width);
+	UNUSED(size);
+	UNUSED(precision);
+
+	if (addrs == NULL)
+		return (write(1, "(nil)", 5));
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
+
+	num_addrs = (unsigned long)addrs;
+
+	while (num_addrs > 0)
+	{
+		buffer[i--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		length++;
+	}
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padd = '0';
+		if (flags & F_PLUS)
+			matrix = '+', length++;
+		else if (flags & F_SPACE)
+		matrix = ' ', length++;
+
+		i++;
+
+		return (write_pointer(buffer, i, length, width, flags, padd, matrix, padd_start));
+}
+
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	int j = 0, offset = 0;
+	char *str = va_arg(types, char *);
+
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		return (write(1, "(null)", 6));
+
+	while (str[j] != '\0')
+	{
+		if (is_printable(str[j]))
+			buffer[j + offset] = str[j];
+		else
+			offset += append_hexa_code(str[j], buffer, j + offset);
+
+		j++;
+	}
+
+	buffer[j + offset] = '\0';
+
+	return (write(1, buffer, j + offset));
+}
+
+int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	char *str;
+	int j, count = 0;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(size);
+
+	str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		UNUSED(precision);
+
+		str = ")Null(";
+	}
+	for (j = 0; str[j]; j++)
+		;
+
+	for (j = j - 1; j >= 0; j--)
+	{
+		char c = str[j];
+
+		write(1, &c, 1);
+		count++;
+	}
+	return (count);
+}
+
+int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	char k, *str;
+	unsigned int n, j;
+	int count = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(types, char *);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	if (str == NULL)
+		str = "(AHYY)";
+	for (n = 0; str[n]; n++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[n])
+			{
+				k = out[j];
+				write(1, &k, 1);
+				count++;
+				break;
+			}
+		}
+
+		if (!in[j])
+		{
+			k = str[n];
+			write(1, &k, 1);
+			count++;
+		}
+	}
+	return (count);
+}
