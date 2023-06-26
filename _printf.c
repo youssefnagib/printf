80613002132022a7f9be45c1711afa3b3b7f44fb
youssefnagib <youssefnagib@yahoo.com>
2023-06-26 12:09:13 +0200

    yalla bena

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ int _printf(const char *format, ...)
 {
 	int l, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	   va_list list;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
