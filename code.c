246ccdf3882a87fde325f5ffe10afe97c0e8b85c
Ma-rwan <marwaneltaieby@hotmail.com>
2023-06-27 18:54:08 +0000

    K

--- code.c
+++ code.c
@@ -1,4 +1,11 @@
 #include "main.h"
+
+/**
+ * is_printable - check if char is printable
+ * @c: evaluated char
+ * Return: 1 if c is printable, 0 if not
+ */
+
 int is_printable(char c)
 {
 	if (c >= 32 && c < 127)
@@ -6,6 +13,15 @@ int is_printable(char c)
 
 	return (0);
 }
+
+/**
+ * append_hexa_code - Append ascci in hexadecimal code
+ * @buffer: Array of chars.
+ * @i: Index at which to start
+ * @ascii_code: ascci code
+ * Return: always 3
+ */
+
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map[] = "0123456789ABCDEF";
@@ -20,6 +36,13 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	return (3);
 }
+
+/**
+ * is_digit - checks if char is a digit
+ * @c: char to be evaluated
+ * Return: 1 if digit, 0 if not
+ */
+
 int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
@@ -27,6 +50,14 @@ int is_digit(char c)
 
 	return (0);
 }
+
+/**
+ *  convert_size_number - cast a num to specified size
+ *  @num: Number to be casted
+ *  @size: Number indecating type
+ *  Return: Casted value
+ */
+
 long int convert_size_number(long int num, int size)
 {
 	if (size == S_LONG)
@@ -36,6 +67,14 @@ long int convert_size_number(long int num, int size)
 
 	return ((int)num);
 }
+
+/**
+ * convert_size_unsgnd - cast a num to specified size
+ *  @num: Number to be casted
+ *  @size: Number indecating type
+ *  Return: Casted value
+ */
+
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
